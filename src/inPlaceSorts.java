6bfe13b3168de04fe65c3b3ff5c01ebaf6daf0f2
BT_1N3_04 <jtan56622bths.edu>
2019-01-09 14:37:44 -0500

    heeeeeeeeeeeeeeeeeeeeeeeeeey ya

--- src/inPlaceSorts.java
+++ src/inPlaceSorts.java
@@ -1,34 +1,33 @@
 public class inPlaceSorts {
 
-    public static void selectionSort(double[] list1 ) {
-        int minPos=0;
-        for(int curPos=0;curPos<list1.length-1;curPos++) {
-            double minV= list1[curPos];
-            minPos= curPos;
-            for(int i= curPos+1; i<list1.length;i++) {
-                if(list1[i]<minV) {
-                    minV= list1[i];
-                    minPos=i;
+    public static void selectionSort(double[] list1) {
+        int minPos = 0;
+        for (int curPos = 0; curPos < list1.length - 1; curPos++) {
+            double minV = list1[curPos];
+            minPos = curPos;
+            for (int i = curPos + 1; i < list1.length; i++) {
+                if (list1[i] < minV) {
+                    minV = list1[i];
+                    minPos = i;
                 }
-                double temp= list1[i];
-                list1[i]= list1[i+1];
-                list1[i+1] = temp;
+                double temp = list1[i];
+                list1[i] = list1[i + 1];
+                list1[i + 1] = temp;
             }
 
         }
 
 
-
     }
 
     public static void bubbleSort(String[] list1) {
-        int x=list1.length;
-        for(int i=0; i<x-1; i++) {
-            for(int j=0; j<x-i-1;j++){
-                if(list1[j].compareTo(list1[j+1]) > 0)  {
-                    String temp= list1[j];
-                    list1[j]= list1[j+1];
-                    list1[j+1] = temp;
+        int x = list1.length;
+        for (int i = 0; i < x - 1; i++) {
+            for (int j = 0; j < x - i - 1; j++) {
+                if (list1[j].compareTo(list1[j + 1]) > 0) {
+                    String temp = list1[j];
+                    list1[j] = list1[j + 1];
+                    list1[j + 1] = temp;
 
                 }
             }
@@ -37,8 +36,26 @@ public class inPlaceSorts {
 
     public static void insertionSort(int[] list1) {
 
+
     }
 
+
+    public String[] randomStringArr(int num, int length) {
+        String[] arr = new String[num];
+        while (num > 0) {
+            int i = 0;
+            String s = "";
+            while (i < length) {
+                char c = (char) ((Math.random() * 26) + 97)
+                s = s + c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
+    }
+}
 
 
 }
